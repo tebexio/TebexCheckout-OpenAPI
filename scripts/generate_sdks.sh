0442b1f1f948390539a3ef81ab4bf56cbb1a8c82
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-05-15 14:42:41 -0500

    remove typescript

--- scripts/generate_sdks.sh
+++ scripts/generate_sdks.sh
@@ -7,8 +7,6 @@ openapi-generator-cli validate -i ../openapi.yaml
 echo "Generating SDKs..."
 openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/python --api-package TebexCheckout --package-name TebexCheckout -g python &
 openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/php --api-package TebexCheckout --package-name TebexCheckout -g php --invoker-package TebexCheckout &
-openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/typescript --api-package TebexCheckout --package-name TebexCheckout -g typescript --invoker-package TebexCheckout &
-openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/ruby --api-package TebexCheckout --package-name TebexCheckout -g ruby --invoker-package TebexCheckout &
 openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/python --api-package TebexCheckout --package-name TebexCheckout -g python --invoker-package TebexCheckout &
 openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/markdown --api-package TebexCheckout --package-name TebexCheckout -g markdown --invoker-package TebexCheckout &
 openapi-generator-cli generate -i ../openapi.yaml -o ../sdks/java --api-package TebexCheckout --package-name TebexCheckout -g java --invoker-package TebexCheckout &
